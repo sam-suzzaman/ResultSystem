a2e1d20b1bba30641da1b7ec51a25664c8e36e91
samsuzzaman1998 <dksamsuzzaman@gmail.com>
2023-08-14 13:59:32 +0600

    add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import "./App.css";
+import Navbar from "./Components/Shared/Navbar/Navbar";
 
 function App() {
     return (
         <>
-            <button class="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900">
-                Button
-            </button>
+            <Navbar />
         </>
     );
 }
---
+++ src/Components/Shared/Navbar/Navbar.jsx
@@ -0,0 +1,49 @@
+import jkkniuLogo from "../../../assets/Jkkniu_logo.png";
+import avatarPhoto from "../../../assets/avatar_photo.jpg";
+import { FiLogOut } from "react-icons/fi";
+
+const Navbar = () => {
+    return (
+        <nav className="shadow-md">
+            <div className="navbar bg-base-100 w-full max-w-7xl mx-auto justify-between">
+                <div className="navbar-start">
+                    <div className="w-12">
+                        <img
+                            className="w-full"
+                            src={jkkniuLogo}
+                            alt="jkkniu_logo"
+                        />
+                    </div>
+                </div>
+                <div className="navbar-center">
+                    <a className="font-bold hidden md:block text-xl uppercase">
+                        Jatiya Kabi Kazi Nazrul Islam University
+                    </a>
+                </div>
+                <div className="navbar-end">
+                    <div className="flex justify-end items-center mr-6">
+                        <img
+                            src={avatarPhoto}
+                            alt="avatar_photo"
+                            className="w-10"
+                        />
+                        <div className="">
+                            <h3 className="text-xs font-medium capitalize mb-0">
+                                User name
+                            </h3>
+                            <p className="text-xs mb-0">Admin</p>
+                        </div>
+                    </div>
+                    <button className="flex justify-end items-center">
+                        <span className="text-[11px] uppercase font-medium mr-1">
+                            logout
+                        </span>
+                        <FiLogOut />
+                    </button>
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
