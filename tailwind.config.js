cce6375440f91748c2582930f744081466989a3b
samsuzzaman1998 <dksamsuzzaman@gmail.com>
2023-08-15 18:03:36 +0600

    remove default theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-    theme: {
-        extend: {},
-    },
+    // theme: {
+    //     extend: {
+    //         mytheme: {
+    //             primary: "#a991f7",
+    //             secondary: "#f6d860",
+    //             accent: "#37cdbe",
+    //             neutral: "#3d4451",
+    //             "base-100": "#ffffff",
+    //         },
+    //     },
+    // },
     plugins: [require("daisyui")],
 };
