f20910b8b7bbc633ca3796537a8f8d1c60fc6db6
samsuzzaman1998 <dksamsuzzaman@gmail.com>
2023-12-09 09:49:28 +0600

    refactor code and update admin sidebar

--- src/Layout/AdminDashboardLayout.jsx
+++ src/Layout/AdminDashboardLayout.jsx
@@ -4,6 +4,7 @@ import { AiFillHome, AiOutlineUsergroupAdd } from "react-icons/ai";
 import { GrDocumentPpt } from "react-icons/gr";
 import { BsBookmarks } from "react-icons/bs";
 import { SessionPageProvider } from "../context/Admin/SessionPageContext";
+import styled from "styled-components";
 
 const AdminDashboardLayout = () => {
     const menu_items = [
@@ -33,48 +34,66 @@ const AdminDashboardLayout = () => {
         },
     ];
     return (
-        <SessionPageProvider>
-            <div className="h-full relative">
-                <div className="drawer lg:drawer-open h-full">
-                    <input
-                        id="Admin_Dashboard_Drawer"
-                        type="checkbox"
-                        className="drawer-toggle"
-                    />
-                    <div className="drawer-content px-6 py-4 overflow-auto">
-                        <Outlet />
-                    </div>
-                    <div className="drawer-side h-full absolute !top-auto">
-                        <label
-                            htmlFor="Admin_Dashboard_Drawer"
-                            className="drawer-overlay h-full"
-                        ></label>
-                        <ul className="menu h-full pl-2 pr-3 p-8 w-[250px] bg-base-200 text-base-content overflow-y-auto flex-nowrap ">
-                            {/* Sidebar content here */}
-                            {menu_items?.map((item) => {
-                                return (
-                                    <li
-                                        key={item.id}
-                                        className="border-b border-slate-300 mb-2 "
-                                    >
-                                        <NavLink
-                                            to={item.path}
-                                            className="capitalize font-semibold rounded-sm"
-                                        >
-                                            <span className="text-xl">
-                                                {item.icon}
-                                            </span>
-                                            {item.title}
-                                        </NavLink>
-                                    </li>
-                                );
-                            })}
-                        </ul>
+        <Wrapper>
+            <SessionPageProvider>
+                <div className="h-full relative">
+                    <div className="drawer lg:drawer-open h-full">
+                        <input
+                            id="Admin_Dashboard_Drawer"
+                            type="checkbox"
+                            className="drawer-toggle"
+                        />
+                        <div className="drawer-content px-6 py-4 overflow-auto">
+                            <Outlet />
+                        </div>
+                        <div className="rms-sidebar drawer-side h-full absolute !top-auto">
+                            <label
+                                htmlFor="Student_Dashboard_Drawer"
+                                className="drawer-overlay h-full"
+                            ></label>
+                            <div className=" h-full  flex flex-col items-center">
+                                {/* <div className="brand">
+                                <img src={deptLogo} alt="eee" />
+                            </div> */}
+                                <ul className="menu pl-2 pr-3 p-8 w-[250px] text-base-content overflow-y-auto flex-nowrap flex-1 bg-base-200">
+                                    {/* Sidebar content here */}
+                                    {menu_items?.map((item) => {
+                                        return (
+                                            <li key={item.id}>
+                                                <NavLink
+                                                    to={item.path}
+                                                    className="capitalize font-medium rms-link"
+                                                >
+                                                    <span className="text-xl">
+                                                        {item?.icon}
+                                                    </span>{" "}
+                                                    {item.title}
+                                                </NavLink>
+                                            </li>
+                                        );
+                                    })}
+                                </ul>
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </SessionPageProvider>
+            </SessionPageProvider>
+        </Wrapper>
     );
 };
 
+const Wrapper = styled.aside`
+    height: 100%;
+
+    .menu .rms-link {
+        border-radius: 4px;
+        margin-bottom: 6px;
+    }
+    .menu .rms-link.active {
+        background-color: rgba(0, 0, 0, 0.06);
+        color: var(--primary-clr);
+        font-weight: 600;
+        letter-spacing: 1px;
+    }
+`;
 export default AdminDashboardLayout;
